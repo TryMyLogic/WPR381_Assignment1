ccee32c113dbae65f01dddba5d6dc9cde380898b
TylerG14 <tyler.geuens@gmail.com>
2025-05-08 13:58:07 +0200

    Added the arrays in the Data.js file. Added css to the events page. Loaded data into the about and events pages(routing), implementation on the about page by Sihle needed(ask for help if needed). lastly I added the event page with text

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { aboutData } = require("../Data.js");
 
+//you're gonna probably have to import the contacts here. I added the team's info to the array that you must integrate with the print on the webpage.
 router.get("/about", (req, res) => {
   res.render("about", { pageName: "about", title: "About us" });
 });
--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { eventsData } = require("../Data.js");
 
 router.get("/events", (req, res) => {
-  res.render("events", { pageName: "events", title: "Upcomming Events" });
+  res.render("events", { pageName: "events", title: "Upcoming Events", events: eventsData });
 });
 
 module.exports = router;
