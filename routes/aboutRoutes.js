a0fc9bdfbef399cc96a714f5c038064dea24f0d4
ZezeWRLD <mthokozisin699@gmail.com>
2025-05-09 13:40:38 +0200

    About page, table loading and basic css

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -4,7 +4,8 @@ const { aboutData } = require("../Data.js");
 
 //you're gonna probably have to import the contacts here. I added the team's info to the array that you must integrate with the print on the webpage.
 router.get("/about", (req, res) => {
-  res.render("about", { pageName: "about", title: "About us" });
+  res.render("about", { pageName: "about", title: "About us", information : aboutData });
 });
 
 module.exports = router;
+console.log("About Data Passed:", aboutData);
